34a4a7df930d8908f6c923bb58fa395970f1b5f3
Leeroy JJ <65292711+Ghostbolt@users.noreply.github.com>
2026-02-06 12:38:22 +0200

    Implement message listener for tab fill action
    Add message listener to run fill on specified tab after delay

--- background.js
+++ background.js
@@ -1,1 +1,11 @@
+// background.js
 
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.type === "RUN_FILL_ON_TAB" && msg.tabId) {
+    // Content script is registered in manifest; we just ping it once tab is ready.
+    // A short delay ensures DOM is present (many frameworks render after load).
+    setTimeout(() => {
+      chrome.tabs.sendMessage(msg.tabId, { type: "FILL_NOW" });
+    }, 2500);
+  }
+});
